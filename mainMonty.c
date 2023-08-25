5915e85718c0b37fe248d07293754704d02b6a79
markrizkalla <mark.rizkalla1@gmail.com>
2023-08-25 17:43:48 +0300

    fix

--- mainMonty.c
+++ mainMonty.c
@@ -14,36 +14,36 @@
 
 int main(int argc, char *argv[])
 {
-        FILE *fileptr;
-        stack_t *stack = NULL;
-        char *buffer = NULL;
-        size_t bufferSize = 0;
-        int i = 1;
-        int mode = 0;
+	FILE *fileptr;
+	stack_t *stack = NULL;
+	char *buffer = NULL;
+	size_t bufferSize = 0;
+	int i = 1;
+	int mode = 0;
 
-        if (argc != 2)
-        {
-                fprintf(stderr, "USAGE: monty file\n");
-                exit(EXIT_FAILURE);
-        }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-        fileptr = fopen(argv[1], "r");
+	fileptr = fopen(argv[1], "r");
 
-        if (fileptr == NULL)
-        {
-                printf("Error: Can't open file %s\n", argv[1]);
-                exit(EXIT_FAILURE);
-        }
+	if (fileptr == NULL)
+	{
+		printf("Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-        while (getline(&buffer, &bufferSize, fileptr) != -1)
-        {
-                processInstruction(i, &stack, buffer, &mode);
-                freeCharPointer(&buffer);
-                i++;
-        }
-        freeCharPointer(&buffer);
-        fclose(fileptr);
-        freeStack(&stack);
+	while (getline(&buffer, &bufferSize, fileptr) != -1)
+	{
+		processInstruction(i, &stack, buffer, &mode);
+		freeCharPointer(&buffer);
+		i++;
+	}
+	freeCharPointer(&buffer);
+	fclose(fileptr);
+	freeStack(&stack);
 
-        return (0);
+	return (0);
 }
